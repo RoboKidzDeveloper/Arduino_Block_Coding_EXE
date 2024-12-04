2c0ad0ca3de6c82ba585b218f7c94cd644abe62d
Robokidz Developers <online@robokidz.co.in>
2024-12-04 10:13:30 +0530

    Removed unwanted files

--- preload.js
+++
@@ -1,8 +0,0 @@
-// preload.js
-const { contextBridge, ipcRenderer } = require('electron');
-
-// Expose ipcRenderer to the renderer process
-contextBridge.exposeInMainWorld('electronAPI', {
-    sendUpload: (data) => ipcRenderer.send('start-upload', data),
-    onUploadProgress: (callback) => ipcRenderer.on('upload-progress', callback)
-});
